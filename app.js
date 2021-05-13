93f0600796cf8058484e25d39639843124b44305
himanshu-learner <himanshumodi@gmail.com>
2021-05-13 21:37:16 +0100

    create one tile movement

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let grid = 3;
+let grid = 4;
+let gameState = 1;
 let gameDiv = document.getElementById("game-div");
 gameDiv.style.height = '450px';
 gameDiv.style.backgroundColor = 'blue';
@@ -31,6 +32,7 @@ function generateGrid() {
                 let gridImg = document.createElement("IMG");
                 gridImg.src = `images/${(i) + grid * (j - 1)}.png`;
                 gridImg.classList.add('img-tag');
+                gridImg.setAttribute('id', `${(i) + grid * (j - 1)}`);
                 gridImg.style.maxWidth = `${gameCellSize - 10}px`;
                 gameCell.appendChild(gridImg);
             }
@@ -39,3 +41,30 @@ function generateGrid() {
 };
 
 generateGrid();
+
+for (let tileSelector = 1; tileSelector<=16; tileSelector++){
+    document.getElementById(tileSelector).addEventListener('click', function(e){
+        console.log(`${tileSelector} tile clicked`);
+    });
+}
+
+let tiles = document.getElementsByClassName('slide-area').addEventListener('click', function(e){
+    console.log('tile clicked');
+});
+
+function tileToMove(row, col) {
+    let c = gameDiv.children[row].children[col].children[0];
+    let dir = 'left';
+    switch(dir) {
+        case 'horizontal':
+            c.style.transform = `translateX(${-gameCellSize}px)`;
+            break;
+        case 'vertical':
+            c.style.transform = `translateX(${gameCellSize}px)`;
+            break;
+        default:
+            console.log('this tile can\'t move'); 
+
+    }
+    c.style.transform = `translateX(${-gameCellSize}px)`;
+}
