e85a9c4e07b5494a64459e5b6a5fd195096be809
himanshu-learner <himanshumodi@gmail.com>
2021-05-14 13:03:49 +0100

    animate one tile

--- app.js
+++ app.js
@@ -8,35 +8,15 @@ ui.generateGrid(gridSize, gameCellSize);
 
 ui.gameDiv.addEventListener('click', function(e){
     //check which tile is clicked
-    console.log(e);
-});
-
-
-
-for (let tileSelector = 1; tileSelector<=16; tileSelector++){
-    document.getElementById(tileSelector).addEventListener('click', function(e){
-        console.log(`${tileSelector} tile clicked`);
-    });
-}
-
-let tiles = document.getElementsByClassName('slide-area').addEventListener('click', function(e){
-    console.log('tile clicked');
-});
-
-function tileToMove(row, col) {
-    let c = gameDiv.children[row].children[col].children[0];
-    let dir = 'left';
-    switch(dir) {
-        case 'horizontal':
-            c.style.transform = `translateX(${-gameCellSize}px)`;
-            break;
-        case 'vertical':
-            c.style.transform = `translateX(${gameCellSize}px)`;
-            break;
-        default:
-            console.log('this tile can\'t move'); 
-
+    if (e.target.id) {
+        tileToMove = e.target.id;
+        //check in game array if there is an empty slot around it and assign dir accordingly
+        let dir = 'left';
+        if (dir) {
+            ui.moveTile(tileToMove, dir);
+        } else {
+            //show alert that tile cannot be moved
+        }
     }
-    c.style.transform = `translateX(${-gameCellSize}px)`;
-}
->>>>>>> tile-move
+});
+
