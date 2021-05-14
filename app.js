6c09f7fb59dfcfd4bcf9b9b09437146ce7f13feb
himanshu-learner <himanshumodi@gmail.com>
2021-05-14 13:39:34 +0100

    create game array

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-let grid = 3;
+let grid = 4;
 let gameDiv = document.getElementById("game-div");
 gameDiv.style.height = '450px';
 gameDiv.style.backgroundColor = 'blue';
 let gameCellSize = Math.floor(360 / grid);
 
+let gameArray = [];
+let tile = 0;
+for (let i = 0; i< grid+1; i++) {
+    gameArray[i] = new Array();
+    for (let j=0; j<grid+2; j++){
+        if (i===0 || j === grid+1 || (j===0 && i !== 1)) {
+            gameArray[i].push('na');
+        } else {
+            gameArray[i].push(tile);
+            tile++
+        }
+    }     
+}
+
+console.log(gameArray);
 function generateGrid() {
     for (let i = 0; i < grid + 2; i++) {
         gridColDiv = document.createElement("div");
@@ -29,7 +44,7 @@ function generateGrid() {
             if (i > 0 && j > 0 && i < grid + 1) {
                 let gameCell = gameDiv.children[i].children[j];
                 let gridImg = document.createElement("IMG");
-                gridImg.src = `images/${(i) + grid * (j - 1)}.png`;
+                gridImg.src = `images/${gameArray[j][i]}.png`;
                 gridImg.classList.add('img-tag');
                 gridImg.style.maxWidth = `${gameCellSize - 10}px`;
                 gameCell.appendChild(gridImg);
