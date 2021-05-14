877a54e4cb9aa74096bec5f909101a61f96a1574
himanshu-learner <himanshumodi@gmail.com>
2021-05-14 10:19:51 +0100

    proceed with adding tile move capability

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const ui = new UI;
 let gameCellSize = Math.floor(360 / gridSize);
 
 ui.generateGrid(gridSize, gameCellSize);
+
+ui.gameDiv.addEventListener('click', function(e){
+    //check which tile is clicked
+    console.log(e);
+});
