e6756434b7ae64093f4b25c891ce80626e8f3870
himanshu-learner <himanshumodi@gmail.com>
2021-05-15 01:41:40 +0100

    finish game core play mechanics

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-let gridSize = 4;
+let gridSize = 5;
 const ui = new UI;
 let gameArray = [];
+let tileMovTracker =[];
 let tile = 0;
-for (let i = 0; i< grid+1; i++) {
+for (let i = 0; i < gridSize + 2; i++) {
     gameArray[i] = new Array();
-    for (let j=0; j<grid+2; j++){
-        if (i===0 || j === grid+1 || (j===0 && i !== 1)) {
+    for (let j = 0; j < gridSize + 2; j++) {
+        if (i === 0 || i === gridSize +1 || j === gridSize + 1 || (j === 0 && i !== 1)) {
             gameArray[i].push('na');
         } else {
             gameArray[i].push(tile);
             tile++
         }
-    }     
+    }
 }
 
-console.log(gameArray);
-
-let gameCellSize = Math.floor(360 / gridSize);
+let gameCellSize = 360/gridSize;
 
 ui.generateGrid(gridSize, gameCellSize);
 
-ui.gameDiv.addEventListener('click', function(e){
+ui.gameDiv.addEventListener('click', function (e) {
     //check which tile is clicked
-    if (e.target.id) {
+    if (parseInt(e.target.id)) {
         tileToMove = e.target.id;
+        let tileIndex = getTileIndex(tileToMove);//get index of tileToMove in gameArray
+        // console.log(`tile index is: ${tileIndex}`);
         //check in game array if there is an empty slot around it and assign dir accordingly
-        let dir = 'left';
-        if (dir) {
+        let dir = getMoveDir(tileIndex);
+        updateGameArray(tileToMove, tileIndex, dir);
+        if (dir != 'ignore') {
             ui.moveTile(tileToMove, dir);
         } else {
             //show alert that tile cannot be moved
@@ -34,3 +36,49 @@ ui.gameDiv.addEventListener('click', function(e){
     }
 });
 
+function getTileIndex(value) {
+    let row, col;
+    for (row = 1; row < gridSize + 1; row++) {
+        col = gameArray[row].indexOf(parseInt(value));
+        if (col !== -1) {
+            return ([row, col]);
+        }
+    }
+}
+
+function getMoveDir(tileIndex) {
+
+    // console.log(gameArray[tileIndex[0]+1][tileIndex[1]])
+    if (tileIndex[0] === 1 && tileIndex[1] === 0 && gameArray[1][1] === 0) {
+        return 'right';
+    } else if (gameArray[tileIndex[0]-1][tileIndex[1]] === 0) {
+        return 'up'
+    } else if ((gameArray[tileIndex[0]+1][tileIndex[1]] === 0)) {
+        return 'down'
+    } else if (gameArray[tileIndex[0]][tileIndex[1]-1] === 0) {
+        return 'left'
+    } else if (gameArray[tileIndex[0]][tileIndex[1]+1] === 0) {
+        return 'right'
+    } else return 'ignore'
+}
+
+function updateGameArray(element, index, dir) {
+    switch (dir) {
+        case 'left':
+            gameArray[index[0]][index[1]-1] = gameArray[index[0]][index[1]];
+            gameArray[index[0]][index[1]] = 0;
+            break;
+        case 'right':
+            gameArray[index[0]][index[1]+1] = gameArray[index[0]][index[1]];
+            gameArray[index[0]][index[1]] = 0;
+            break;
+        case 'up':
+            gameArray[index[0]-1][index[1]] = gameArray[index[0]][index[1]];
+            gameArray[index[0]][index[1]] = 0;
+            break;
+        case 'down':
+            gameArray[index[0]+1][index[1]] = gameArray[index[0]][index[1]];
+            gameArray[index[0]][index[1]] = 0;
+            break;
+    }
+}
