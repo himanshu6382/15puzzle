500d2a64c28a6662ac28a54be588f19d4675cde7
himanshu-learner <himanshumodi@gmail.com>
2021-05-21 22:47:31 +0100

    complete slider v1.0

--- app.js
+++ app.js
@@ -2,25 +2,36 @@ let gameState = 0;
 let gridSize = 3;
 const ui = new UI;
 let gameArray = [];
-let tileMovTracker = [];
+const tileMovTracker = [];
 let tile = 0;
 let gameCellSize = 360 / gridSize;
+let moves = 0;
 createGameArray();
-scramble();
 ui.generateGrid(gridSize);
+ui.statusMessage('freeplay');
 
 ui.gameDiv.addEventListener('click', function (e) {
     //check which tile is clicked
     if (parseInt(e.target.id)) {
-        tileToMove = e.target.id;
-        let tileIndex = getTileIndex(tileToMove);//get index of tileToMove in gameArray
+        let tileToMove = e.target.id;
+        let tileIndex = getTileIndex(parseInt(tileToMove), gameArray);//get index of tileToMove in gameArray
         //check in game array if there is an empty slot around it and assign dir accordingly
         let dir = getMoveDir(tileIndex);
-        updateGameArray(tileToMove, tileIndex, dir);
+        updateGameArray(tileIndex, dir);
         if (dir != 'ignore') {
+            if (gameState === 1) {
+                moves+=1;
+                let gameComplete = checkGame();
+                if (gameComplete) {
+                    ui.statusMessage('win');
+                    gameState = 0;
+                } else {
+                    ui.statusMessage(dir);
+                }
+            }
             ui.moveTile(tileToMove, dir);
         } else {
-            //show alert that tile cannot be moved
+            if (gameState===1) ui.statusMessage(dir);
         }
     }
 });
@@ -31,16 +42,43 @@ document.getElementById('game-init').addEventListener('click', function (e) {
         let option = size.children[0];
         if (option.checked) gridSize = parseInt(option.value);
     }
-    initiateGameVariables();
+    initiateGame();
 });
 
-function initiateGameVariables(){
+document.addEventListener('keyup', (e) => {
+    let key = e;
+    let keyboardMov = tileToMoveOnKeyPress(e.key);;
+    if (keyboardMov.dirToMove) {
+        let tileToMove = gameArray[keyboardMov.tileToMove[0]][keyboardMov.tileToMove[1]];
+        updateGameArray(keyboardMov.tileToMove, keyboardMov.dirToMove);
+        if (gameState ===1) {
+            moves+=1;
+            let gameComplete = checkGame();
+            if (gameComplete) {
+                ui.statusMessage('win');
+                gameState = 0;
+            } else {
+                ui.statusMessage(keyboardMov.dirToMove)
+            }
+        }
+        ui.moveTile(tileToMove, keyboardMov.dirToMove);
+    }
+});
+
+function initiateGame(){
     gameCellSize = 360 / gridSize;
+    moves = 0;
     tile = 0;
+    gameState = 1;
     createGameArray();
     scramble();
+    checkSolvability();
+    console.log(`Game score is ${gameScore()}`)
     ui.clearGrid();
     ui.generateGrid(gridSize);
+    ui.statusMessage();
+    
+    
 }
 
 function createGameArray() {
@@ -57,10 +95,10 @@ function createGameArray() {
     }
 }
 
-function getTileIndex(value) {
+function getTileIndex(value, game) {
     let row, col;
     for (row = 1; row < gridSize + 1; row++) {
-        col = gameArray[row].indexOf(parseInt(value));
+        col = game[row].indexOf(value);
         if (col !== -1) {
             return ([row, col]);
         }
@@ -83,7 +121,7 @@ function getMoveDir(tileIndex) {
     } else return 'ignore'
 }
 
-function updateGameArray(element, index, dir) {
+function updateGameArray(index, dir) {
     switch (dir) {
         case 'left':
             gameArray[index[0]][index[1] - 1] = gameArray[index[0]][index[1]];
@@ -106,24 +144,35 @@ function updateGameArray(element, index, dir) {
 
 function scramble() {
 //create an array with values of 1 to the total numbers in the grid
-
+    let parity = 0;
     let tempArray = [];
     for (let i = 0; i<gridSize*gridSize; i++) {
         tempArray.push(i+1);
     }
 
-//randomly arrange the numbers in the above array
-//randomly select any index between 2 to the highest number. 
-//For e.g. say grid size is 3x3. So tiles will be from 1 to 9
-//loop from last tile to tile 2, exchanging with any randomly chosen tile(index) from tile 2 to 9.
-//first iteration: exchange tempArray[8] with tempArray [index between 2 and 9]. In 2nd iter, exchange tempArray[7], with something between 2 and 7
+//randomly arrange the numbers in the above array - index from 0 to gridSize^2-1
+//randomly select any index between 2 to the highest number.
+//For e.g. say grid size is 3x3. So tiles will be from 1 to 9. Stored in tempArray[0,1,..,8]
+//loop on tempArray. For tempArray[8], swap it with any randomly chosen tile between tempArray[1] and tempArray[8].
+//Then swap tempArray[7] with any randomly chosen tile between tempArray[1] and tempArray[7].
+//So on till you reach tile tempArray[2]. Here you have to check parity of the scrambled puzzle.
+//The puzzle will be solvable only for even parity - i.e. if number of swaps are even in number. So check for parity. If it is even swap tempArray[1] and tempArray[2]
 //remember 1st tile should always be 1. So don't include tempArray[0] in the loop
 
-    for (let i = gridSize*gridSize-1; i>0; i--) {
-        valToExchange = Math.floor(Math.random()*i+1);
-        let temp = tempArray[i];
-        tempArray[i] = tempArray[valToExchange];
-        tempArray[valToExchange] = temp;
+    for (let i = gridSize*gridSize-1; i>1; i--) {
+        if (i>2) {
+            valToExchange = Math.floor(Math.random()*i+1);
+            let temp = tempArray[i];
+            tempArray[i] = tempArray[valToExchange];
+            tempArray[valToExchange] = temp;
+            if (valToExchange != i) parity+=1;
+        } else {
+            if (parity%2 != 0) {
+                temp = tempArray[i];
+                tempArray[i] = tempArray[1];
+                tempArray[1] = temp;
+            }
+        }
     }
 
 //assign the above randomized temparray to game array
@@ -137,3 +186,99 @@ function scramble() {
 }
 //if using image, keep a selection button on top to revert to numbered tiles
 //also eliminate borders between tiles if playing with custom image
+
+function checkGame() {
+    for (i=1; i<=gridSize; i++) {
+        for (j=1; j<=gridSize; j++) {
+            if( gameArray[i][j] !== j+ (i-1)*gridSize) return(false);
+        }
+    }
+    return(true);    
+}
+
+function gameScore() {
+    let score = 0;
+    for (i = 1; i<= gridSize; i++) {
+        for (j = 1; j<=gridSize; j++){
+            let currPos = getTileIndex(j+(i-1)*gridSize, gameArray);
+            score = score + Math.abs(currPos[0] - i) + Math.abs(currPos[1] - j);
+        }
+    }
+    return score;
+}
+
+function scrambleTest() {
+    gridSize = 3;
+    gameArray[0] = ['na', 'na', 'na', 'na', 'na'];
+    gameArray[1] = [0, 1, 5, 3, 'na'];
+    gameArray[2] = ['na', 2, 4, 6, 'na'];
+    gameArray[3] = ['na', 7, 8, 9, 'na'];
+    gameArray[4] = ['na', 'na', 'na', 'na', 'na'];
+}
+
+// function scrambleTest() {
+//     gridSize = 3;
+//     gameArray[0] = ['na', 'na', 'na', 'na', 'na'];
+//     gameArray[1] = [0, 1, 5, 3, 'na'];
+//     gameArray[2] = ['na', 2, 4, 6, 'na'];
+//     gameArray[3] = ['na', 7, 8, 9, 'na'];
+//     gameArray[4] = ['na', 'na', 'na', 'na', 'na'];
+// }
+
+function tileToMoveOnKeyPress(key) {
+    let emptyIndex = getTileIndex(0, gameArray);
+    let dirToMove;
+    let tileToMove = []
+    switch (key) {
+        case 'ArrowLeft':
+            if (gameArray[emptyIndex[0]][emptyIndex[1]+1] != 'na') {
+                dirToMove = 'left';
+                tileToMove = [emptyIndex[0], emptyIndex[1]+1]
+            }
+            break;
+        case 'ArrowRight':
+            if (gameArray[emptyIndex[0]][emptyIndex[1]-1] != 'na') {
+                dirToMove = 'right';
+                tileToMove = [emptyIndex[0], emptyIndex[1]-1]
+            }
+            break;
+        case 'ArrowUp':
+            if (gameArray[emptyIndex[0]+1][emptyIndex[1]] != 'na') {
+                dirToMove = 'up';
+                tileToMove = [emptyIndex[0]+1, emptyIndex[1]]
+            }
+            break;
+        case 'ArrowDown':
+            if (gameArray[emptyIndex[0]-1][emptyIndex[1]] != 'na') {
+                dirToMove = 'down';
+                tileToMove = [emptyIndex[0]-1, emptyIndex[1]]
+            }
+            break;
+    }
+    return {
+        dirToMove,
+        tileToMove
+    };
+}
+
+function checkSolvability() {
+    let parity = 0;
+    let checkArray = new Array();
+    for (i = 1; i<=gridSize; i++){
+        checkArray[i] = Array.from(gameArray[i]);
+    }
+    for (i = 1; i<=gridSize; i++) {
+        for (j = 1; j<=gridSize; j++) {
+            if (!(i===1 && j===1)) {
+               let currTile = checkArray[i][j];
+               let corrTilePos = getTileIndex(j+(i-1)*gridSize, checkArray);
+               if (!(corrTilePos[0] === i && corrTilePos[1] === j)) {
+                   checkArray[corrTilePos[0]][corrTilePos[1]] = currTile;
+                   checkArray[i][j] = j+(i-1)*gridSize;
+                   parity+=1;
+               } 
+            }
+        }
+    }
+    (parity%2 === 0)? console.log("solvable"): console.log("unsolvable");
+}
--- ui.js
+++ ui.js
@@ -8,7 +8,6 @@ class UI {
     }
 
     generateGrid(grid) {
-        console.log("game generated");
         for (let i = 0; i < grid + 2; i++) {
             let gridColDiv = document.createElement("div");
             gridColDiv.classList.add('grid-col');
@@ -38,28 +37,76 @@ class UI {
                     gridImg.setAttribute('id', `${gameArray[j][i]}`);
                     gridImg.style.maxWidth = `${gameCellSize-5}px`;
                     gameCell.appendChild(gridImg);
-                    tileMovTracker[gameArray[j][i]]=[0,0];
+                    tileMovTracker[gameArray[j][i]]={
+                        xAxis: 0,
+                        yAxis: 0
+                    };
                 }
             }
         }
     }
 
+    //logic as follows:
+    //every tile's starting position has x and y coordinate as 0, 0
+    //translate moves each tile in increments of the size of the grid - w.r.t to the starting position
+    //tileMovTracker tracks how much left, right, up or down each tile has moved w.r.t to its starting position.
+    //tileMovTracker is a nested array. Each array item is an object tracking x-coordinate and y-coordinate of each tile
+    //there surely is a less complex way to do this?
+
+
     moveTile(tileToMove, dir) {
         let tile = document.getElementById(tileToMove);
         switch(dir) {
             case 'left':
-                tileMovTracker[tileToMove][0]-=1;
+                tileMovTracker[tileToMove].xAxis-=1;
                 break;
             case 'right':
-                tileMovTracker[tileToMove][0]+=1;
+                tileMovTracker[tileToMove].xAxis+=1;
                 break;
             case 'up':
-                tileMovTracker[tileToMove][1]-=1;
+                tileMovTracker[tileToMove].yAxis-=1;
                 break;
             case 'down':
-                tileMovTracker[tileToMove][1]+=1;
+                tileMovTracker[tileToMove].yAxis+=1;
                 break;
         }
-        tile.style.transform = `translate(${tileMovTracker[tileToMove][0]*gameCellSize}px,${tileMovTracker[tileToMove][1]*gameCellSize}px)`;
+        tile.style.transform = `translate(${tileMovTracker[tileToMove].xAxis*gameCellSize}px,${tileMovTracker[tileToMove].yAxis*gameCellSize}px)`;
+    }
+
+    statusMessage(message) {
+        let statusDiv = document.getElementsByClassName('features')[0];
+        let divToInsert = document.createElement('div');
+        divToInsert.classList.add('status-message');
+        switch(message) {
+            case 'freeplay':
+                divToInsert.innerHTML = `
+                <p>Click on "Start New Game" for a new game</p>`;
+                statusDiv.appendChild(divToInsert);
+                break;
+            case 'ignore':
+                if (statusDiv.children[1]) this.statusMessageHide();
+                divToInsert.style.backgroundColor = 'teal';
+                divToInsert.style.color = 'white';
+                divToInsert.innerHTML = `
+                <p>Clicked tile cannot be moved</p>`;
+                statusDiv.appendChild(divToInsert);
+                setTimeout(this.statusMessageHide, 2000);
+                break;
+            case 'win':
+                if (statusDiv.children[1]) this.statusMessageHide();
+                divToInsert.innerHTML = `
+                <p>Game won in ${moves} moves. Click on "Start New Game" for a new game</p>`;
+                statusDiv.children[0].innerHTML = `Moves: ${moves}`;
+                statusDiv.appendChild(divToInsert);
+                break;
+            default:
+                if (statusDiv.children[1]) this.statusMessageHide();
+                statusDiv.children[0].innerHTML = `Moves: ${moves}`;
+        }
+    }
+
+    statusMessageHide() {
+        let statusMessage = document.getElementsByClassName('features')[0].children[1];
+        if (statusMessage) statusMessage.parentNode.removeChild(statusMessage);
     }
 }  
